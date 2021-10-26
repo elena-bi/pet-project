090b4f2a2f3d020743486284d43a1fc811209de9
elena-bi <bisericescuelena@gmail.com>
2021-10-27 02:15:09 +0300

    Add: className and event handler for logo

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -27,7 +27,7 @@ function Navbar() {
         <React.Fragment>
             <nav className="navbar">
                 <div className="navbar-container">
-                    <Link to='/'> Travelium <i class="fas fa-plane-departure"></i></Link>
+                    <Link to='/' className='navbar-logo' onClick={closeMobileMenu}> Travelium <i class="fas fa-plane-departure"></i></Link>
                 </div>
                 <div className="menu-icon" onClick={handleClick}> 
                     <i className={click ? "fas fa-times" : "fas fa-bars"} />
