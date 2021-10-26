7b7586ffb8950ee019f9d3b67d42f8057d966cf6
elena-bi <bisericescuelena@gmail.com>
2021-10-27 01:35:09 +0300

    Add: Cases when thewindow is mobile size

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -4,10 +4,22 @@ import {Link } from "react-router-dom";
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
 
     const closeMobileMenu = () => setClick(false)
+
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    window.addEventListener('resize', showButton);
 
     return (
         <React.Fragment>
