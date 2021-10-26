9bdd7e43e75983c2d0a7df7bc9b3f8a5aba1907c
elena-bi <bisericescuelena@gmail.com>
2021-10-26 18:02:43 +0300

    Add: Navbar logo and menu icon

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
 
 function Navbar() {
     return (
-        <div>
-            
-        </div>
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to='/'> Travelium <i class="fas fa-plane-departure"></i></Link>
+                </div>
+                <div className="menu-icon"> <i class="fas fa-bars"></i></div>
+            </nav>
+        </>
     )
 }
 
 export default Navbar
+
