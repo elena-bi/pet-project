3ec6eea47d107398f2f196faaae1246870ee22fd
elena-bi <bisericescuelena@gmail.com>
2021-10-27 01:10:08 +0300

    Add: State change for icons and links with respective path

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import React from 'react'
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-        Cleaned project
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact></Route>
+        </Switch>
+      </Router>
+      
     </div>
   );
 }
--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,17 +1,42 @@
-import React from 'react'
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import React, { useState } from 'react';
+import {Link } from "react-router-dom";
 
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+
+    const closeMobileMenu = () => setClick(false)
+
     return (
-        <>
+        <React.Fragment>
             <nav className="navbar">
                 <div className="navbar-container">
                     <Link to='/'> Travelium <i class="fas fa-plane-departure"></i></Link>
                 </div>
-                <div className="menu-icon"> <i class="fas fa-bars"></i></div>
+                <div className="menu-icon" onClick={handleClick}> 
+                    <i className={click ? "fas fa-times" : "fas fa-bars"} />
+                </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li>    
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}> 
+                            Home
+                        </Link>
+                    </li>
+                    <li>    
+                        <Link to='/products' className='nav-links' onClick={closeMobileMenu}> 
+                            Products
+                        </Link>
+                    </li>
+                    <li>    
+                        <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}> 
+                            Sign Up
+                        </Link>
+                    </li>
+                </ul>
             </nav>
-        </>
+        </React.Fragment>
     )
 }
 
