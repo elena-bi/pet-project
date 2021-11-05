0d1ddce44557f77247f4f70c34a05fc944781860
elena-bi <bisericescuelena@gmail.com>
2021-11-05 14:16:16 +0200

    Update: Dependencies, packages, App component and syling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,29 @@
+import React from 'react';
+// import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-import React from 'react'
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+// import Image from '../src/images/background-1.jpg'
+import ImageSlider from './components/ImageSlider';
+import { SliderData } from './components/SliderData';
+
+const image = { uri: "https://reactjs.org/logo-og.png" };
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
+
+        <ImageSlider slides={SliderData} />
+
         <Switch>
           <Route path='/' exact></Route>
         </Switch>
       </Router>
-      
     </div>
   );
 }
+
 
 export default App;
