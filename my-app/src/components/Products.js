9b4d7ee2ecbf0e9e42615254dc2eaef434ad7b58
elena-bi <bisericescuelena@gmail.com>
2021-11-23 14:54:49 +0200

    Update: Products.js
    Separating concerns. Products container is not responsible for how the products are displayed

--- my-app/src/components/Products.js
+++ my-app/src/components/Products.js
@@ -1,22 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar.js'
-import CardContent from './Card.js'
-import { Container, Row, Col } from 'react-bootstrap'
-import { ProductsData } from './ProductsData.js';
+import CardInformation from './CardInformation'
 
 function Products() {
     return (
         <div>
             <Navbar />
-            <Container>
-                <Row >
-                    {ProductsData.map(props => (
-                        <Col lg={3}>
-                            <CardContent key={props.id} props={props} />
-                        </Col>
-                    ))}
-                </Row>
-            </Container>
+
+            <CardInformation/>
 
         </div>
     )
